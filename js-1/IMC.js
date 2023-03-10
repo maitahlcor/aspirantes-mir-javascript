c7c5e96addb9decf28e6f2670a3ddaff1dbf7b2e
Andres Lopez <73659007+maitahlcor@users.noreply.github.com>
2023-03-10 18:34:05 -0500

    IMC.js

---
+++ js-1/IMC.js
@@ -0,0 +1,14 @@
+function bmi(peso, talla) {
+    try{
+        let imc = (peso)/(talla^2)
+        console.log(`Su IMC es ${imc}`);
+        return imc
+    }catch(e){
+        console.log(e);
+    }
+    
+  }
+
+  bmi(65 , 1.8) ;
+  bmi(72, 1.6) ;
+  bmi(52, 1.75) ;
